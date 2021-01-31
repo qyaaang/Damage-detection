34be78f0b18e95a527c48ee3b627b453b629df48
Qun Yang <yangqunstruct@gmail.com>
2021-01-31 21:52:00 +1300

    Add wavelet transform denoise

--- denoise.m
+++ denoise.m
@@ -1,14 +1,14 @@
 % @Author: qyan327
 % @Date:   2021-01-31 18:46:32
 % @Last Modified by:   qyan327
-% @Last Modified time: 2021-01-31 21:09:34
+% @Last Modified time: 2021-01-31 21:42:18
 
 
 %% denoise: Wavelet transform denoise
 function [xd] = demoise(x)
-	figure;
-	plot(x);
+	% figure;
+	% plot(x);
 	xd = wdenoise(x, 4);
-	hold on;
-	plot(xd);
-	legend('Noisy signal','Denoised Signal')
+	% hold on;
+	% plot(xd);
+	% legend('Noisy signal','Denoised Signal')

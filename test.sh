6039426044a6cfa76e32eb10dcd169eb7ae0e014
Qun Yang <yangqunstruct@gmail.com>
2021-02-14 18:23:49 +1300

    Add test

---
+++ test.sh
@@ -0,0 +1,43 @@
+sys=$(uname -a)
+Mac="Darwin"
+len_segs=(300)
+datasets=("W-2" "W-5" "W-7")
+net_names=("MLP")
+num_hidden_maps=(256)
+num_epoch=10000
+learning_rate=1e-4
+for len_seg in "${len_segs[@]}"; do
+  for net_name in "${net_names[@]}"; do
+    for dataset in "${datasets[@]}"; do
+      if [ "$net_name" == "MLP" ]; then
+          printf "\033[1;32mDataset:\t%s\nLength of segments:\t%s\nNet name:\t%s\n\033[0m" \
+                 "$dataset" "$len_seg" "$net_name"
+          if [[ $sys =~ $Mac ]]; then
+              python3 test.py --dataset "$dataset" --model_name AE --net_name "$net_name" \
+                              --len_seg "$len_seg" \
+                              --num_epoch $num_epoch --learning_rate $learning_rate
+          else
+              python test.py --dataset "$dataset" --model_name AE --net_name "$net_name" \
+                             --len_seg "$len_seg" \
+                             --num_epoch $num_epoch --learning_rate $learning_rate
+          fi
+      else
+          for num_hidden_map in "${num_hidden_maps[@]}"; do
+            printf "\033[1;32mDataset:\t%s\nLength of segments:\t%s\nNet name:\t%s\nNum hidden maps:\t%s\n\033[0m" \
+                   "$dataset" "$len_seg" "$net_name" "$num_hidden_map"
+            if [[ $sys =~ $Mac ]]; then
+               python3 test.py --dataset "$dataset" --model_name AE --net_name "$net_name" \
+                               --len_seg "$len_seg" \
+                               --num_epoch $num_epoch --num_hidden_map "$num_hidden_map" \
+                               --learning_rate $learning_rate
+            else
+               python test.py --dataset "$dataset" --model_name AE --net_name "$net_name" \
+                              --len_seg "$len_seg" \
+                              --num_epoch $num_epoch --num_hidden_map "$num_hidden_map" \
+                              --learning_rate $learning_rate
+            fi
+          done
+      fi
+    done
+  done
+done
